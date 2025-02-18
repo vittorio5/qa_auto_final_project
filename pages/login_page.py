0a244a274f03c3c06385ca9058ecf3ddb421f271
vittorio5 <vittorio.inbox@gmail.com>
2025-02-18 23:22:25 +0700

    login page tests added

---
+++ pages/login_page.py
@@ -0,0 +1,22 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        login_url = self.browser.current_url        
+        # реализуйте проверку на корректный url адрес
+        assert "login" in login_url, "Подстрока login не найдена в ссылке"
+
+    def should_be_login_form(self):
+        # реализуйте проверку, что есть форма логина
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
+
+    def should_be_register_form(self):
+        # реализуйте проверку, что есть форма регистрации на странице
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is not presented"
--- test_main_page.py
+++ test_main_page.py
@@ -1,4 +1,6 @@
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
+import time
 
 
 def test_guest_can_go_to_login_page(browser):
@@ -6,9 +8,12 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
     page.open()                      # открываем страницу
     page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
+    log_link = browser.current_url #"http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    loginpage = LoginPage(browser, log_link)
+    loginpage.should_be_login_page()
 
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
-    page.should_be_login_link()
+    page.should_be_login_link()    
